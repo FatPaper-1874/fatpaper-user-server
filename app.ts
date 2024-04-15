d8ffb3a24d8801436d95888d41456e9a23dc0469
FatPaper-1874 <360655753@qq.com>
2024-04-15 15:46:52 +0800

    添加了/health接口，配合docker-compose的按顺序启动

--- app.ts
+++ app.ts
@@ -4,39 +4,45 @@ import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
 import routerUser from "./src/routers/user";
-import { serverLog } from "./src/utils/logger";
+import {serverLog} from "./src/utils/logger";
 import chalk from "chalk";
-import { TOKENKEY, APIPORT } from "./src/static";
+import {TOKENKEY, APIPORT} from "./src/static";
 
 async function bootstrap() {
-	try {
-		await AppDataSource.initialize().then(() => {
-			serverLog(`${chalk.bold.bgGreen(" 数据库连接成功 ")}`);
-		});
+    try {
+        await AppDataSource.initialize().then(() => {
+            serverLog(`${chalk.bold.bgGreen(" 数据库连接成功 ")}`);
+        });
 
-		// await redisClientInit().then(() => {
-		// 	serverLog(`${chalk.bold.bgGreen(" Redis连接成功 ")}`);
-		// });
+        // await redisClientInit().then(() => {
+        // 	serverLog(`${chalk.bold.bgGreen(" Redis连接成功 ")}`);
+        // });
 
-		const app = express();
+        const app = express();
 
-		app.use(cors());
+        app.use(cors());
 
-		app.use("/static", express.static("public"));
+        app.use("/static", express.static("public"));
 
-		// app.use(roleValidation);
+        // app.use(roleValidation);
 
-		app.use(bodyParser.json());
+        app.use(bodyParser.json());
 
-		app.use("/user", routerUser);
+        app.use("/user", routerUser);
 
-		app.listen(APIPORT, () => {
-			serverLog(`${chalk.bold.bgGreen(` API服务启动成功 ${APIPORT}端口 `)} `);
-		});
-	} catch (e: any) {
-		serverLog(`${chalk.bold.bgRed(` 服务器出错: `)}`, "error");
-		console.log(e)
-	}
+        app.get('/health', (req, res) => {
+            // 在这里进行服务的健康检查，返回适当的响应
+            // 为了配合docker-compose按顺序启动
+            res.status(200).send('OK');
+        });
+
+        app.listen(APIPORT, () => {
+            serverLog(`${chalk.bold.bgGreen(` API服务启动成功 ${APIPORT}端口 `)} `);
+        });
+    } catch (e: any) {
+        serverLog(`${chalk.bold.bgRed(` 服务器出错: `)}`, "error");
+        console.log(e)
+    }
 }
 
 bootstrap();
