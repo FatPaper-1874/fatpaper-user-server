a34eb7e75e9a5b391b8cd150238c99a3cffb1ce6
FatPaper-1874 <360655753@qq.com>
2024-08-01 15:44:56 +0800

    优化

--- src/utils/db/dbConnecter.ts
+++ src/utils/db/dbConnecter.ts
@@ -1,12 +1,12 @@
 import {DataSource} from "typeorm";
-import {MYSQL_PORT} from "../../../../global.config";
+import {MYSQL_PASSWORD, MYSQL_PORT, MYSQL_USERNAME} from "../../../../global.config";
 
 const AppDataSource = new DataSource({
     type: "mysql",
     host: process.env.NODE_ENV == "production" ? "mysql" : "localhost",
     port: MYSQL_PORT,
-    username: "root",
-    password: "root",
+    username: MYSQL_USERNAME,
+    password: MYSQL_PASSWORD,
     database: "fatpaper_user",
     synchronize: true,
     entities: [__dirname + "/entities/*{.js,.ts}"],
