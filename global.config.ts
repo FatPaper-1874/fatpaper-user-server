6f7c51ab74947c794279e41e96eac5a808852b6f
FatPaper-1874 <360655753@qq.com>
2024-09-25 02:15:34 +0800

    优化配置文件global.config.ts, 提高灵活度

--- global.config.ts
+++ global.config.ts
@@ -1,14 +1,14 @@
 import {
-    __TC_SECRETID__ as tc_id,
-    __TC_SECRETKEY__ as tc_key,
-    __TC_BUCKET_NAME__ as tc_bn,
-    __TC_REGION__ as tc_r
-} from "../tencent-cloud"
-import {FATPAPER_HOST, FATPAPER_USER_SERVER_PORT} from "../global.config";
+	__TC_SECRETID__ as tc_id,
+	__TC_SECRETKEY__ as tc_key,
+	__TC_BUCKET_NAME__ as tc_bn,
+	__TC_REGION__ as tc_r,
+} from "../tencent-cloud";
+import { USER_SERVER_URL } from "../global.config";
 
 export const __TC_ID__ = tc_id;
 export const __TC_KEY__ = tc_key;
 export const __TC_BUCKET_NAME__ = tc_bn;
 export const __TC_REGION__ = tc_r;
 
-export const __FATPAPERUSERSERVERHOST__ = `${FATPAPER_HOST}:${FATPAPER_USER_SERVER_PORT}`
+export const __FATPAPERUSERSERVERHOST__ = USER_SERVER_URL;
